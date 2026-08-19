7f0a37cb2d5c5806af17915ba50f4945d22d4fe5
anand23vs <132248672+anand-gec@users.noreply.github.com>
2026-08-20 00:55:44 +0530

    gfdsa

--- for_funn/pointersfun.go
+++ for_funn/pointersfun.go
@@ -3,21 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	var name string= "Vikash"
-	fmt.Println("name is nothing changed",name)  //name is nothing changed Vikash
+	var name string = "Vikash"
+	fmt.Println("name is nothing changed", name)            //name is nothing changed Vikash
+	fmt.Println("address of name in without change", &name) // address of name in without change 0x2498e3b1c030
 
 	ptr := &name
 	sec_ptr := &ptr
 
 	*ptr = "Ranjesh"
-	fmt.Println("name is ", name)  //name is  Ranjesh
-	fmt.Println("name is &ptr", *ptr)  //name is &ptr Ranjesh
-	fmt.Println("name is &ptr", &ptr)  //name is &ptr 0x35cae6dea060
-	
-	**sec_ptr = "Ajay"
-	fmt.Println("name is ", name)  //name is  Ajay
-	fmt.Println("name is &ptr", *ptr)  //name is &ptr Ajay
+	fmt.Println("name is ", name)     //name is  Ranjesh
+	fmt.Println("name is &ptr", *ptr) //name is &ptr Ranjesh
 	fmt.Println("name is &ptr", &ptr) //name is &ptr 0x35cae6dea060
+
+	**sec_ptr = "Ajay"
+	fmt.Println("name is ", name)          //name is  Ajay
+	fmt.Println("name is &ptr", *ptr)      //name is &ptr Ajay
+	fmt.Println("name is &ptr", &ptr)      //name is &ptr 0x35cae6dea060
 	fmt.Println("name is &ptr", **sec_ptr) //name is &ptr Ajay
-	fmt.Println("name is &ptr", &sec_ptr) //name is &ptr 0x35cae6dea068
+	fmt.Println("name is &ptr", &sec_ptr)  //name is &ptr 0x35cae6dea068
 }
